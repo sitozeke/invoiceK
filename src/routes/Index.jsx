18edf106f2464a0e27a934115cb20265ff6752d4
sitozeke <sitozeke@gmail.com>
2023-09-19 22:35:24 +0100

    fixes

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/auth/SignIn";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     children: [
